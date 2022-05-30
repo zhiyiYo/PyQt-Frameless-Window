aedef3a1919efdc7a05ea56d0e6b59ac9fa65671
zhiyiYo <1319158137@qq.com>
2022-05-30 20:20:43 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PyQt5-Frameless-Window",
-    version="0.0.3",
+    version="0.0.4",
     keywords="pyqt frameless",
     author="Huang Zhengzhi",
     author_email="1319158137@qq.com",
