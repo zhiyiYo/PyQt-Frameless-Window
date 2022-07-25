19d6cde19476a25bcb1bc46dfbf0c45907d2d07f
zhiyiYo <1319158137@qq.com>
2022-07-25 09:19:58 +0800

    重构标题栏

--- qframelesswindow/__init__.py
+++ qframelesswindow/__init__.py
@@ -1,17 +1,16 @@
 import sys
 
+from .titlebar import TitleBar
+
 if sys.platform == "win32":
-    from .titlebar import WindowsTitleBar as TitleBar
     from .windows import AcrylicWindow
     from .windows import WindowsFramelessWindow as FramelessWindow
     from .windows import WindowsWindowEffect as WindowEffect
 elif sys.platform == "darwin":
-    from .titlebar import MacTitleBar as TitleBar
     from .mac import AcrylicWindow
     from .mac import MacFramelessWindow as FramelessWindow
     from .mac import MacWindowEffect as WindowEffect
 else:
-    from .titlebar import LinuxTitleBar as TitleBar
     from .linux import LinuxFramelessWindow as FramelessWindow
     from .linux import LinuxWindowEffect as WindowEffect
 
