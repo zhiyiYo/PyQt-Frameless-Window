92f56a7c523c7b6629b98ff3b88fdbb889581be3
zhiyiYo <1319158137@qq.com>
2022-10-19 19:21:03 +0800

    取消标题栏的默认透明属性

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PyQt5-Frameless-Window",
-    version="0.1.0",
+    version="0.1.1",
     keywords="pyqt frameless",
     author="Huang Zhengzhi",
     author_email="1319158137@qq.com",
