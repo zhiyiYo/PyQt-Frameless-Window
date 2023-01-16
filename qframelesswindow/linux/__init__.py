fd205736b233c2bb4ba2caaa717b9243407cbb76
zhiyiYo <1319158137@qq.com>
2023-01-16 20:14:07 +0800

    添加是否启用调整大小的方法

--- qframelesswindow/linux/__init__.py
+++ qframelesswindow/linux/__init__.py
@@ -17,6 +17,7 @@ class LinuxFramelessWindow(QWidget):
         super().__init__(parent=parent)
         self.windowEffect = LinuxWindowEffect(self)
         self.titleBar = TitleBar(self)
+        self._isResizeEnabled = True
 
         self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
         QCoreApplication.instance().installEventFilter(self)
@@ -41,9 +42,13 @@ class LinuxFramelessWindow(QWidget):
         self.titleBar.setParent(self)
         self.titleBar.raise_()
 
+    def setResizeEnabled(self, isEnabled: bool):
+        """ set whether resizing is enabled """
+        self._isResizeEnabled = isEnabled
+
     def eventFilter(self, obj, event):
         et = event.type()
-        if et != QEvent.MouseButtonPress and et != QEvent.MouseMove:
+        if et != QEvent.MouseButtonPress and et != QEvent.MouseMove or not self._isResizeEnabled:
             return False
 
         edges = Qt.Edges()
