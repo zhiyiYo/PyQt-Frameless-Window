b5a77ae89c657494a643bf9eb56a360cd2c35c67
zhiyiYo <1319158137@qq.com>
2023-03-28 11:34:56 +0800

    更换许可证

--- qframelesswindow/__init__.py
+++ qframelesswindow/__init__.py
@@ -9,10 +9,10 @@ online at https://pyqt-frameless-window.readthedocs.io.
 Examples are available at https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/Pyside2/examples.
 
 :copyright: (c) 2021 by zhiyiYo.
-:license: MIT, see LICENSE for more details.
+:license: LGPLv3, see LICENSE for more details.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 import sys
 
--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PySide2-Frameless-Window",
-    version="0.0.4",
+    version="0.0.5",
     keywords="pyside2 frameless",
     author="zhiyiYo",
     author_email="shokokawaii@outlook.com",
     description="A cross-platform frameless window based on pyside2, support Win32, Linux and macOS.",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    license="MIT",
+    license="LGPLv3",
     url="https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/Pyside2",
     packages=setuptools.find_packages(),
     install_requires=[
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: OS Independent'
     ]
 )
