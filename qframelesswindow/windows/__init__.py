8aa764b4dffa7116ed5199de718d2a2fa8d73755
Cheukfung <joephenhe@gmail.com>
2023-04-29 23:57:08 +0800

    修复 Windows7 下无边框对话框报错的问题 (#90)

--- qframelesswindow/windows/__init__.py
+++ qframelesswindow/windows/__init__.py
@@ -32,9 +32,10 @@ class WindowsFramelessWindow(QWidget):
         # remove window border
         if not win_utils.isWin7():
             self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+        elif parent:
+            self.setWindowFlags(parent.windowFlags() | Qt.FramelessWindowHint | Qt.WindowMinMaxButtonsHint)
         else:
-            self.setWindowFlags(Qt.FramelessWindowHint |
-                                Qt.WindowMinMaxButtonsHint)
+            self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowMinMaxButtonsHint)
 
         # add DWM shadow and window animation
         self.windowEffect.addWindowAnimation(self.winId())
@@ -149,8 +150,12 @@ class AcrylicWindow(WindowsFramelessWindow):
         self.__closedByKey = False
 
         QtWin.enableBlurBehindWindow(self)
-        self.setWindowFlags(Qt.FramelessWindowHint |
-                            Qt.WindowMinMaxButtonsHint)
+        
+        if win_utils.isWin7() and parent:
+            self.setWindowFlags(parent.windowFlags() | Qt.FramelessWindowHint | Qt.WindowMinMaxButtonsHint)
+        else:
+            self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowMinMaxButtonsHint)
+        
         self.windowEffect.addWindowAnimation(self.winId())
 
         if win_utils.isWin7():
