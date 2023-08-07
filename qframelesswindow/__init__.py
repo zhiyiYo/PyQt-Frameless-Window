f1b1ac327ac009a7255c7b5ca1ed2951f820b9e2
zhiyiYo <1319158137@qq.com>
2023-08-07 15:16:03 +0800

    添加标题栏基类 `TitleBarBase`

--- qframelesswindow/__init__.py
+++ qframelesswindow/__init__.py
@@ -12,13 +12,13 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/
 :license: GPLv3, see LICENSE for more details.
 """
 
-__version__ = "0.2.9"
+__version__ = "0.3.0"
 
 import sys
 
 from PyQt5.QtWidgets import QDialog, QMainWindow
 
-from .titlebar import TitleBar, TitleBarButton, SvgTitleBarButton, StandardTitleBar
+from .titlebar import TitleBar, TitleBarButton, SvgTitleBarButton, StandardTitleBar, TitleBarBase
 
 if sys.platform == "win32":
     from .windows import AcrylicWindow
