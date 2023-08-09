74196f38b5f50fbcb30e336b50e31f1640ed61f4
zhiyiYo <1319158137@qq.com>
2023-08-09 09:22:39 +0800

    修复切换 Mica 主题无效的问题 (#112)

--- qframelesswindow/__init__.py
+++ qframelesswindow/__init__.py
@@ -12,7 +12,7 @@ Examples are available at https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/
 :license: GPLv3, see LICENSE for more details.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 import sys
 
