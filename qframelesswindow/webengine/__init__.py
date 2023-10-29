7a2c22068833739d2ec44b01f38aa9cd3488cadd
zhiyiYo <1319158137@qq.com>
2023-10-29 15:38:39 +0800

    修复 QWebEngineView 父级问题

--- qframelesswindow/webengine/__init__.py
+++ qframelesswindow/webengine/__init__.py
@@ -1,10 +1,20 @@
+# coding: utf-8
+import sys
+
+from PySide2.QtCore import Qt
 from PySide2.QtWebEngineWidgets import QWebEngineView
+from qframelesswindow import AcrylicWindow, FramelessWindowBase
 
 
 class FramelessWebEngineView(QWebEngineView):
     """ Frameless web engine view """
 
-    def __init__(self, parent=None):
+    def __init__(self, parent):
+        if sys.platform == "win32" and isinstance(parent.window(), AcrylicWindow):
+            parent.window().setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
         super().__init__(parent=parent)
         self.setHtml("")
-        self.window().updateFrameless()
+
+        if isinstance(self.window(), FramelessWindowBase):
+            self.window().updateFrameless()
